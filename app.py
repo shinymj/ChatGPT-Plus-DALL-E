80aba256c5006698d7a3a70d86c916cbc945bfc9
Minjung Shin <99765384+shinymj@users.noreply.github.com>
2023-05-21 22:07:24 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import openai
+
+openai.api_key = st.secrets["api_key"]
+
+st.title ("chatGPT Plus DALL-E")
+
+with st.form("form"): 
+    user_input = st.text_input("Prompt")
+    size = st.selectbox("Size", ["1024x1024", "512x512", "256x256"])
+    submit = st.form_submit_button("Submit")
+
+if submit and user_input:    
+    gpt_prompt = [{
+        "role": "system",
+        "content": "imagine the detail appearance of the input. Response it shortly around 20 words"
+    }]
+    gpt_prompt.append({
+        "role": "user",
+        "content": user_input
+    })
+
+    with st.spinner("Waiting for chatGPT..."):
+        gpt_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=gpt_prompt
+        )
+
+
+    prompt = gpt_response["choices"][0]["message"]["content"]
+    st.write(prompt)
+
+    with st.spinner("Waiting for Dall-E..."):
+        dalle_response = openai.Image.create(
+            prompt = prompt,
+            size = size
+        )
+
+    st.image(dalle_response["data"][0]["url"])
